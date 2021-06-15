61f6fa65676476812a4be7476526a7b7fb225bfd
lh <jyfc7879@gmail.com>
2021-06-15 16:18:23 +0800

    ..

--- src/main/java/com/lh/springboottemplate/SpringBootTemplateApplication.java
+++ src/main/java/com/lh/springboottemplate/SpringBootTemplateApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootTemplateApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringBootTemplateApplication.class, args);
 	}
 
